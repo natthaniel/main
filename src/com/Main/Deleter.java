8dda28e5a9dca532eb0a1c2db8ef8138695377bd
Kai Xiang <kaixiang91@gmail.com>
2016-03-22 13:42:57 +0800

    Updated Deleter to ensure that it is working with new architecture.
    Also, ensure that the user deletes what they see on the screen

--- src/com/Main/Deleter.java
+++ src/com/Main/Deleter.java
@@ -9,12 +9,13 @@ public class Deleter implements Commander{
 	public Deleter(String[] parsedUserInput, ArrayList<Task> TaskList){
 		//The first element in the array is the index to be deleted
 		indexToDelete = Integer.parseInt(parsedUserInput[0]);
+		this.TaskList = TaskList;
 	}
 	@Override
 	public String execute() {
 		try{
 		//assert(indexToDelete >=0);
-		TaskList.remove(indexToDelete);
+		TaskList.remove(indexToDelete-1);
 		} catch (Exception e){
 			e.printStackTrace();
 			return e.toString();
