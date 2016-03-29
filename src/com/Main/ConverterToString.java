7c6220ce0f090a03f4adf32aeabcd1acae806981
Nathaniel <nathanielzyc@gmail.com>
2016-03-29 18:04:27 +0800

    write specification for ConverterToString and SearcherForFreeTimeSlots Classes

---
+++ src/com/Main/ConverterToString.java
@@ -0,0 +1,13 @@
+package com.Main;
+/*
+ * This class can take in either a single Task object or an arrayList of them.
+ * Then an object of ConverterToString will convert the Task object(s) into a formatted string.
+ * Such string will be returned to the caller.
+ */
+public class ConverterToString {
+
+	public ConverterToString() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/Main/SearcherForFreeTimeSlot.java
@@ -0,0 +1,23 @@
+package com.Main;
+/*
+ * This class takes in the original TaskList and a desired time slot as input parameter for its constructor
+ * the desired time slot should be in the form of a string array.
+ * The 1st element of the string array contain the start time 
+ * The 2st element of the string array contain the start time 
+ * E.g.: [1000][1400] (looking for a free time slot from 10am to 2pm)
+ * The return value is a string, indicating the date of earliest day that can take in the desired time slot.
+ * for V0.3, this function will only return the earliest day, NOT considering later available day.
+ */
+public class SearcherForFreeTimeSlot implements Commander {
+
+	public SearcherForFreeTimeSlot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
