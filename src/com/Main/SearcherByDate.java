d3f467d786cefd66a75853fcf0ef9ec347accf39
Nathaniel <nathanielzyc@gmail.com>
2016-03-29 18:03:41 +0800

    write specification for ConverterToString and SearcherForFreeTimeSlots Classes

--- src/com/Main/SearcherByDate.java
+++ src/com/Main/SearcherByDate.java
@@ -6,8 +6,8 @@ public class SearcherByDate implements Commander {
 	 * This class will take in a date string in the form of "MM dd yyyy"
 	 * and it will search from the TaskList for Tasks that has start-time on that date.
 	 * Then it will get an arrayList of Task objects fulfilling the requirement.
-	 * Before return, it will call the Display Class with the arrayList with the parameter.
-	 * The Display Object will return a string to the processor for display.
+	 * Before return, it will call the ConverterToString Class with the arrayList with the parameter.
+	 * The ConverterToString Object will return a string to the processor for display.
 	 * 
 	 */
 	public SearcherByDate() {
--- src/com/Main/SearcherByKeyword.java
+++ src/com/Main/SearcherByKeyword.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
  * This class will take in a string of Keyword.
  * and it will search from the TaskList for desired Tasks.
  * Then it will get an arrayList of Task objects fulfilling the requirement.
- * Before return, it will call the Display Class with the arrayList as the parameter.
- * The Display Object will return a string to the processor for display.
+ * Before return, it will call the ConverterToString Class with the arrayList as the parameter.
+ * The ConverterToString Object will return a string to the processor for display.
  */
 public class SearcherByKeyword implements Commander {
 	private String keyWord;
